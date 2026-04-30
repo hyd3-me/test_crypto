151db6ce5c7b003d626d2d0cca4a219aed876bb1
sedg <hyde.rou@yandex.ru>
2026-04-30 20:54:56 +0300

    test: add failing test for PIN_WARNING

--- tests/test_constants.py
+++ tests/test_constants.py
@@ -4,3 +4,10 @@ import app.constants as constants
 def test_fixed_message():
     expected = "Bytestream v1: Generate messaging keys for this device."
     assert constants.FIXED_MESSAGE == expected
+
+
+def test_pin_warning():
+    expected = (
+        "WARNING: You have set a PIN code. You must remember it to recover your keys."
+    )
+    assert constants.PIN_WARNING == expected
