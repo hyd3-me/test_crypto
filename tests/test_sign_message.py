8419869166bd692a483d9e316c4c4999d039d2cb
sedg <hyde.rou@yandex.ru>
2026-04-30 20:30:17 +0300

    test: sign_message with same PIN is deterministic

--- tests/test_sign_message.py
+++ tests/test_sign_message.py
@@ -17,3 +17,11 @@ def test_compose_sign_message_with_pin():
     message = compose_sign_message(FIXED_MESSAGE, pin)
     assert "WARNING" in message
     assert "PIN: 1234" in message
+
+
+def test_sign_message_same_pin_identical(sign_message):
+    message1 = compose_sign_message(FIXED_MESSAGE, "secret")
+    message2 = compose_sign_message(FIXED_MESSAGE, "secret")
+    sig1 = sign_message(message1)
+    sig2 = sign_message(message2)
+    assert sig1 == sig2
