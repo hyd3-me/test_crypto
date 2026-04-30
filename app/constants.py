0f46c59d44a58acf0802232c1c878d12cf58c74b
sedg <hyde.rou@yandex.ru>
2026-04-30 21:02:41 +0300

    feat: add PIN_LABEL constant

--- app/constants.py
+++ app/constants.py
@@ -2,3 +2,4 @@ FIXED_MESSAGE = "Bytestream v1: Generate messaging keys for this device."
 PIN_WARNING = (
     "WARNING: You have set a PIN code. You must remember it to recover your keys."
 )
+PIN_LABEL = "PIN: "
