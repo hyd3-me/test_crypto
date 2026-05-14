3929519b9fab905e8ccaa5b3b624dafa778a5ebc
sedg <hyde.rou@yandex.ru>
2026-05-14 17:40:09 +0300

    test: add failing test for compute_content_hash deterministic

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,3 +6,12 @@ def test_get_project_root():
     root = utils.get_project_root()
     env_path = os.path.join(root, ".env")
     assert os.path.isfile(env_path)
+
+
+def test_compute_content_hash_deterministic():
+    msg_id = b"\x00" * 12
+    content = b"hello"
+    h1 = utils.compute_content_hash(msg_id, content)
+    h2 = utils.compute_content_hash(msg_id, content)
+    assert h1 == h2
+    assert isinstance(h1, bytes)
